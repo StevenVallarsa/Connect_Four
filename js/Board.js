3df85731975bd6e8e26e927b0ee41875e061cacc
StevenVallarsa <orionsjv@gmail.com>
2020-11-17 18:25:20 -0500

    Added new classes and funcationality but not yet tested

--- js/Board.js
+++ js/Board.js
@@ -1,3 +1,23 @@
 class Board {
-    
+    constructor() {
+        this.rows = 6;
+        this.columns = 7;
+        this.spaces = this.createSpaces();
+    }
+
+    /**
+     * Generates a 2D array of spaces
+     * @return {Array} An array of space objects
+     */
+    createSpaces() {
+        const board = [];
+        for (let i = 0; i < this.columns; i++) {
+            const column = [];
+            for (let j = 0; j < this.rows; i++) {
+                column.push(new Space(i, j));
+            }
+            board.push(column);
+        }
+        return board;
+    }
 }
--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,25 @@
 class Game {
-    
+    constructor() {
+        this.board = new Board();;
+        this.players = this.createPlayers();
+        this.ready = false;
+    }
+
+    /**
+     * Generates 2 player objects
+     * @return {Array} array of two player objects
+     */
+    createPlayers() {
+        return [
+            new Player("Player 1", 1, "#e15258", true),
+            new Player("Player 2", 2, "#e59a13")
+        ];
+    }
+
+    /**
+     * Starts game
+     */
+    startGame() {
+        
+    }
 }
